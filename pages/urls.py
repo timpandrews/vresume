1d0c9515a6d718508a35b5312e463cd27e5b3706
Tim Andrews <timpandrews@yahoo.com>
2022-02-20 22:47:16 -0500

    Build Static Templates

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
+    path('', views.index, name='index'),
 }
--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return render(request, 'pages/home.html')
+def index(request):
+    return render(request, 'pages/index.html')
 
-
-def about(request):
-    return render(request, 'pages/about.html')
