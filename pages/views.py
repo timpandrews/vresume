2a41b842386a6357c2a8f95eac7713881387aa35
Tim Andrews <timpandrews@yahoo.com>
2022-02-20 21:35:17 -0500

    Settup Static

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
