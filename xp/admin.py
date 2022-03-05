267c79a3b359d9093c122d97a5f5b74cc892f4ec
Tim Andrews <timpandrews@yahoo.com>
2022-03-04 19:09:21 -0500

    XP Admin Settings

--- xp/admin.py
+++ xp/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from xp.models import Xp
 
 class XpAdmin(admin.ModelAdmin):
-    list_display = ('id', 'hero_image')
-    list_display_links = ('id', )
-    verbose_name = "Xp"
+    list_display = ('id', 'title', 'slug', 'author', 'status', 'hero_image',)
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    list_display_links = ('id', 'title', 'slug',)
+    prepopulated_fields = {'slug': ('title',)}
+    verbose_name = 'Xp'
 
 admin.site.register(Xp, XpAdmin)
