bf27617e2d816117b6766612b5a43d5072b2d8d2
Tim Andrews <timpandrews@yahoo.com>
2022-03-06 16:13:16 -0500

    Adding XP Sidebar

--- xp/admin.py
+++ xp/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from xp.models import Xp
 
 class XpAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'author', 'status', 'hero_image',)
+    list_display = ('id', 'title', 'slug', 'author', 'status', 'created_at', 'updated_at', 'hero_image',)
     list_filter = ('status',)
     search_fields = ['title', 'content']
     list_display_links = ('id', 'title', 'slug',)
     prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at', 'updated_at')
     verbose_name = 'Xp'
 
 admin.site.register(Xp, XpAdmin)
